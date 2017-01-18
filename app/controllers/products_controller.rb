91007a1c65e900798bc29b747dd336bb7734b9c9
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-01-17 21:27:39 -0500

    Project with Gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,9 @@ class ProductsController < ApplicationController
   @sauce = Product.first
   render "hotsauce_display.html.erb"
   end
+
+  def all_sauce
+    @allsauce = Product.all
+    render "all_hotsauces.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  get "/first_sauce", to: "products#first_sauce" 
+ get "/all_sauces", to: "products#all_sauce"
 end
