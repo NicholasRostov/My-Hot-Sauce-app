40a5bad47811388914689237125a76e4151146ab
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-01-24 20:56:46 -0500

    Adding model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,21 @@
 class Product < ApplicationRecord
-def time
-created_at.strftime("%b %d, %Y")
-end
+  def time
+    created_at.strftime("%b %d, %Y")
+  end
 
+  def sale_massage
+    if price.to_i < 2
+      return "Discount massage!"
+    else 
+      return "Everyday value massage!"
+    end
+  end
 
+  def tax
+   round = price[1..-1].to_f * 0.09
+  end
+
+  def total
+    tax + price[1..-1].to_f
+  end
 end
